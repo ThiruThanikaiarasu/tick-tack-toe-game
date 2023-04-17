c6e7f6db8becf53e5eb80e6cd9565ce479f0eb97
Thirunavukkarasu T <97720002+thiru042004@users.noreply.github.com>
2023-04-17 11:00:13 +0530

    Initial commit done, to save file structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+// import GameComponent from './GameComponent';
+import GameComponent from './components/GameComponent/GameComponent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GameComponent/>
   );
 }
 
---
+++ src/components/GameComponent/GameComponent.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class GameComponent extends Component {
+  render() {
+    return (
+      <div>GameComponent</div>
+    )
+  }
+}
+
+export default GameComponent
